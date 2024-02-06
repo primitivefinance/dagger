dc2b3afe18d7a1a6b60eaf68419fe0cd9d081070
clemlak <clementlakhal@gmail.com>
2024-02-06 16:52:21 +0400

    feat: update pools list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { useAccount } from "wagmi";
+import { getNonce } from "../../services/dfmm";
+
 function Home() {
+  const account = useAccount();
+
+  useEffect(() => {
+    (async () => {
+      console.log(account);
+
+      if (account.isConnected) {
+        console.log(await getNonce());
+      } else {
+        console.log("Not connected");
+      }
+    })();
+  }, [account.isConnected]);
+
   return (
     <>
       <div className="container mx-auto my-8">
@@ -13,10 +31,10 @@ function Home() {
         <table>
           <thead>
             <tr>
-              <th>Composition</th>
-              <th className="text-right">Strategy</th>
+              <th className="normal-case">Composition</th>
+              <th className="text-right normal-case">Strategy</th>
               <th className="text-right">TVL</th>
-              <th className="text-right">Volume 24h</th>
+              <th className="text-right normal-case">Volume 24h</th>
             </tr>
           </thead>
           <tbody style={{ borderTopWidth: '1px' }}>
@@ -24,8 +42,8 @@ function Home() {
               <td>
                 <div className="flex flex-row items-center gap-2">
                   <div className="flex flex-row items-center">
-                    <img src="https://raw.githubusercontent.com/balancer/tokenlists/main/src/assets/images/tokens/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png" alt="WETH" className="rounded-full size-8" />
-                    <img src="https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png" alt="USDC" className="rounded-full size-8" />
+                    <img src="https://raw.githubusercontent.com/balancer/tokenlists/main/src/assets/images/tokens/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png" alt="WETH" className="rounded-full size-8" style={{ zIndex: 1 }} />
+                    <img src="https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png" alt="USDC" className="rounded-full size-8" style={{ marginLeft: '-8px' }} />
                   </div>
                   WETH/USDC
                 </div>
