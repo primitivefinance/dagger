c5d984effe154433b363dbb3e8db5c362498c784
clemlak <clementlakhal@gmail.com>
2024-02-08 16:18:12 +0400

    feat: add connect wallet in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,10 @@
+import {
+  useConnectModal,
+} from '@rainbow-me/rainbowkit';
+
 function Header() {
+  const { openConnectModal } = useConnectModal();
+
   return (
     <nav className="flex flex-row items-center justify-between py-2 px-5 border-b border-black border-solid">
       <div className="flex-grow w-full flex-row flex gap-4">
@@ -13,7 +19,7 @@ function Header() {
         </a>
       </div>
       <div className="flex-grow w-full flex-row-reverse flex gap-4">
-        <button className="p-2 bg-[#000000] text-white">Connect Wallet</button>
+        <button className="p-2 bg-[#000000] text-white" onClick={openConnectModal}>Connect Wallet</button>
         <button className="p-2 bg-[#000000] text-white">Ethereum</button>
       </div>
     </nav>
