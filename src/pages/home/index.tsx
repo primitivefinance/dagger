35bd28693fcb909240ff3fd7007edf59c3d42af8
clemlak <clementlakhal@gmail.com>
2024-02-12 18:54:45 +0400

    feat: update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,8 +9,8 @@ const FakePool = () => (
         <div className="flex flex-col font-bold">
           WETH/USDC
           <div className="flex flex-row gap-2">
-            <div className="bg-blue-600 px-2 rounded-full text-[14px]">0.03%</div>
-            <div className="bg-purple-600 px-2 rounded-full text-[14px]">G3M</div>
+            <div className="bg-blue-600 px-2 rounded-full text-xs">0.03%</div>
+            <div className="bg-purple-600 px-2 rounded-full text-xs">G3M</div>
           </div>
         </div>
       </div>
@@ -26,18 +26,29 @@ const FakePool = () => (
 function Home() {
   return (
     <>
-      <div className="container mx-auto my-16 max-w-4xl">
-        <div className="max-w-md gap-6 flex flex-col">
-          <h1>Manage your funds lorem strategies.</h1>
-          <h3 className="leading-tight text-dagger3">
-            Manage your funds onchain, stack too deep and you might get liquidated, stack too shallow and you might miss out on rewards.
-          </h3>
+      <div
+        className="w-full py-16"
+      >
+        <div className="container mx-auto max-w-4xl">
+          <div className="max-w-md gap-4 flex flex-col">
+            <h1>Manage your funds lorem strategies.</h1>
+            <h3 className="leading-tight text-dagger3 font-normal">
+              Manage your funds onchain, stack too deep and you might get liquidated, stack too shallow and you might miss out on rewards.
+            </h3>
+          </div>
         </div>
       </div>
-      <div className="container mx-auto my-8 max-w-4xl gap-2 flex flex-col">
+      <div className="container mx-auto max-w-4xl gap-2 flex flex-col">
         <div className="flex flex-row items-center w-full justify-between">
           <h3>All pools (42)</h3>
-          <button className="p-2">Create pool</button>
+          <button className="p-2">
+            <div className="flex flex-row items-center gap-1">
+              <svg className="w-4 h-3 text-dagger4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 12h14m-7 7V5" />
+              </svg>
+              Create pool
+            </div>
+          </button>
         </div>
         <div className="bg-dagger1 rounded-lg border border-dagger2 border-solid">
           <table>
