cc95335b3f26c42f9439a783f49e9ad03637ef96
clemlak <clementlakhal@gmail.com>
2024-02-13 16:34:19 +0400

    feat: update header buttons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,7 @@ function Header() {
   const { openConnectModal } = useConnectModal();
 
   return (
-    <nav className="flex flex-row items-center justify-between py-1 px-5 border-b border-dagger2 border-solid">
+    <nav className="flex flex-row items-center justify-between py-2 px-5 border-b border-dagger2 border-solid">
       <div className="flex-grow w-full flex-row flex gap-8 items-center">
         <a href="/" className="hover:no-underline">
           🗡️
@@ -30,13 +30,13 @@ function Header() {
             <svg className="w-4 h-4 text-dagger4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
             </svg>
-            Connect Wallet
+            <span className="text-sm">Connect Wallet</span>
           </div>
         </button>
         <button>
           <div className="flex flex-row gap-1 items-center">
             <img src="https://info.uniswap.org/static/media/optimism.34412af263c903b58c43a243c4a21d56.svg" alt="OP" className="rounded-full size-4" />
-            Optimism
+            <span className="text-sm">Optimism</span>
           </div>
         </button>
       </div>
