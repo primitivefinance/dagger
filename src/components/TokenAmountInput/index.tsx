3d53d2469236f8747ef36d32a2e4f8b05668a7c5
clemlak <clementlakhal@gmail.com>
2024-02-15 18:49:19 +0400

    feat: add balance set to TokenAmountInput

--- src/components/TokenAmountInput/index.tsx
+++ src/components/TokenAmountInput/index.tsx
@@ -31,7 +31,10 @@ function TokenAmountInput(props: TokenAmountInputProps) {
           <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <path className="text-dagger3 group-hover:text-dagger4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
           </svg>
-          <p className="text-sm text-dagger3 group-hover:text-dagger4">
+          <p
+            className="text-sm text-dagger3 group-hover:text-dagger4"
+            onClick={() => props.setAmount(props.tokenBalance.toString())}
+          >
             {props.tokenBalance.toLocaleString(undefined)} {props.tokenSymbol}
           </p>
         </div>
