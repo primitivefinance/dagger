7dbd100e7259ef35dd49c9f59f90de2ca4a45eae
clemlak <clementlakhal@gmail.com>
2024-02-15 16:36:10 +0400

    feat: add computeL for G3M

--- src/lib/g3m.ts
+++ src/lib/g3m.ts
@@ -6,3 +6,9 @@ export function computeAndFormatPrice(reserveX: number, reserveY: number, weight
   const price = computePrice(reserveX, reserveY, weightX, weightY);
   return isNaN(price) ? '0.0' : price.toLocaleString(undefined, { maximumFractionDigits: 8, minimumFractionDigits: 2 });
 }
+
+export function computeL(x: bigint, y: bigint, wX: bigint, wY: bigint): bigint {
+  const a = x ** wX;
+  const b = y ** wY;
+  return a * b;
+}
