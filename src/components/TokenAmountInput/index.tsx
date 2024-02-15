8aa47ccaf772b5a963a130d2b02a8f3dcf2721b7
clemlak <clementlakhal@gmail.com>
2024-02-15 12:32:54 +0400

    feat: add amount format in TokenAmountInput

--- src/components/TokenAmountInput/index.tsx
+++ src/components/TokenAmountInput/index.tsx
@@ -32,7 +32,7 @@ function TokenAmountInput(props: TokenAmountInputProps) {
             <path className="text-dagger3 group-hover:text-dagger4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
           </svg>
           <p className="text-sm text-dagger3 group-hover:text-dagger4">
-            {props.tokenBalance} {props.tokenSymbol}
+            {props.tokenBalance.toLocaleString(undefined)} {props.tokenSymbol}
           </p>
         </div>
       </button>
