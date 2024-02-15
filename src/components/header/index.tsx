8b58ed210924757bcfd3cfd23b308e49c9351285
clemlak <clementlakhal@gmail.com>
2024-02-15 10:27:30 +0400

    feat: add connect injected wallet

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,9 @@
+import { useAccount, useConnect } from 'wagmi';
+
 function Header() {
+  const { connectors, connect } = useConnect();
+  const { address } = useAccount();
+
   return (
     <nav className="flex flex-row items-center justify-between py-2 px-5">
       <div className="flex-grow w-full flex-row flex gap-8 items-center">
@@ -19,12 +24,21 @@ function Header() {
         </a>
       </div>
       <div className="flex-grow w-full flex-row-reverse flex gap-4 items-center">
-        <button onClick={() => { }}>
+        <button onClick={() => connect({ connector: connectors[0] })}>
           <div className="flex flex-row gap-1 items-center">
-            <svg className="w-4 h-4 text-dagger4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
-            </svg>
-            <span className="text-sm">Connect Wallet</span>
+            {address !== undefined ? (
+              <>
+                <div className="w-4 h-4 rounded-full bg-red-500" />
+                <span className="text-sm">{address.slice(0, 6)}...{address.slice(address.length - 4, address.length)}</span>
+              </>
+            ) : (
+              <>
+                <svg className="w-4 h-4 text-dagger4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
+                </svg>
+                <span className="text-sm">Connect Wallet</span>
+              </>
+            )}
           </div>
         </button>
         <button>
