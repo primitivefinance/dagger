eb5635d2897cb547c6fd4007b0802c6540c002a9
clemlak <clementlakhal@gmail.com>
2024-02-15 18:06:39 +0400

    feat: fix SVG bad attribute names

--- src/components/TokenSelector/index.tsx
+++ src/components/TokenSelector/index.tsx
@@ -36,7 +36,7 @@ function TokenSelector(props: TokenSelectorProps) {
             </div>
             <div className="flex flex-row gap-2 bg-dagger1 border-dagger2 border border-solid p-2 rounded-lg items-center">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <path className="text-dagger3" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+                <path className="text-dagger3" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
               </svg>
               <input
                 type="text"
