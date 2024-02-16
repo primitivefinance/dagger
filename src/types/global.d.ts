005c51c44e5ad9ab15f210ca9d54f2580941233a
clemlak <clementlakhal@gmail.com>
2024-02-16 15:42:12 +0400

    feat: add Pool, Token and G3MParameters types

---
+++ src/types/global.d.ts
@@ -0,0 +1,28 @@
+type Pool = {
+  id: bigint;
+  tokenX: Token;
+  tokenY: Token;
+  reserveX: bigint;
+  reserveY: bigint;
+  liquidity: bigint;
+  timestamp: bigint;
+  name: string;
+  lpToken: `0x${string}`;
+  parameters: G3MParameters;
+};
+
+type Token = {
+  name: string;
+  symbol: string;
+  decimals: number;
+  address: `0x${string}`
+}
+
+type G3MParameters = {
+  id: bigint;
+  swapFee: bigint;
+  weightX: bigint;
+  weightY: bigint;
+  controller: string;
+}
+
