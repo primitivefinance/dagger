485ccd367f00c37eaa093dcb9e05a355a1ecbae7
clemlak <clementlakhal@gmail.com>
2024-02-16 19:02:43 +0400

    feat: update types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,13 +2,20 @@ type Pool = {
   id: bigint;
   tokenX: Token;
   tokenY: Token;
-  reserveX: bigint;
-  reserveY: bigint;
-  liquidity: bigint;
+  reserveX: number;
+  reserveXWad: bigint;
+  reserveYWad: bigint;
+  reserveY: number;
+  liquidity: number;
+  liquidityWad: bigint;
   timestamp: bigint;
   name: string;
   lpToken: `0x${string}`;
   parameters: G3MParameters;
+  strategy: Strategy;
+  positions: {
+    items: Position[];
+  }
 };
 
 type Token = {
@@ -20,9 +27,22 @@ type Token = {
 
 type G3MParameters = {
   id: bigint;
-  swapFee: bigint;
-  weightX: bigint;
-  weightY: bigint;
+  swapFeeWad: bigint;
+  swapFee: number;
+  weightXWad: bigint;
+  weightX: number;
+  weightY: number;
+  weightYWad: bigint;
   controller: string;
 }
 
+type Strategy = {
+  name: string;
+  id: `0x${string}`;
+}
+
+type Position = {
+  liquidityWad: bigint;
+  liquidity: number;
+  accountId: `0x${string}`;
+}
