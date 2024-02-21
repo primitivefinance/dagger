93ead2095fd97243bf7a4be5cf6e212b440d4028
clemlak <clementlakhal@gmail.com>
2024-02-21 11:15:37 +0400

    feat: add Modal component

---
+++ src/components/WalletConnector/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { useAccount, useConnect } from 'wagmi';
+
+import Modal from '../Modal';
+
+function getButtonState(
+  address: string | undefined,
+  isConnecting: boolean,
+  toggleModal: (state: boolean) => void,
+) {
+  if (isConnecting) {
+    return (
+      <button>
+        <div className="flex flex-row gap-1 items-center">
+          <svg className="w-4 h-4 text-dagger4 animate-spin" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21 12a9 9 0 1 1-6.219-8.56" />
+          </svg>
+          <span className="text-sm">Connecting</span>
+        </div>
+      </button>
+    );
+  } else if (address === undefined) {
+    return (
+      <button onClick={() => toggleModal(true)}>
+        <div className="flex flex-row gap-1 items-center">
+          <svg className="w-4 h-4 text-dagger4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
+          </svg>
+          <span className="text-sm">Connect Wallet</span>
+        </div>
+      </button>
+    );
+  } else {
+    return (
+      <button>
+        <div className="flex flex-row gap-1 items-center">
+          <span className="text-sm">{address?.slice(0, 6)}...{address?.slice(address.length - 4, address.length)}</span>
+        </div>
+      </button>
+    );
+  }
+}
+
+function WalletSelection() {
+  const { connectors, connect } = useConnect();
+  const { address } = useAccount();
+
+  const [isModalOpen, toggleModal] = useState(false);
+  const [isConnecting, toggleConnecting] = useState(false);
+
+  return (
+    <>
+      <Modal
+        isOpen={isModalOpen}
+        toggle={toggleModal}
+        title="Select a wallet"
+      >
+        <div className="flex flex-col gap-4">
+          {connectors.map((connector) => (
+            <button
+              key={connector.uid}
+              onClick={() => {
+                toggleConnecting(true);
+                connect({
+                  connector,
+                }, {
+                  onSuccess: () => toggleModal(false),
+                  onSettled: () => toggleConnecting(false),
+                });
+              }}
+            >
+              <div className="flex flex-row gap-2 items-center">
+                <img src={`/${connector.name}.svg`} alt={connector.name} className="w-6" />
+                {connector.name}
+              </div>
+            </button>
+          ))}
+        </div>
+      </Modal>
+      {getButtonState(address, isConnecting, toggleModal)}
+    </>
+  )
+}
+
+export default WalletSelection;
