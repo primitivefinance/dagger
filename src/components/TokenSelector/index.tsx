ff38a848aa19b0f633297df4c2d8b87290b7c840
Kinrezc <matt.czernik@gmail.com>
2024-03-22 15:24:51 -0400

    fix token selection modal

--- src/components/TokenSelector/index.tsx
+++ src/components/TokenSelector/index.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import { tokens } from '../../data/tokens';
+import { Dialog, DialogTrigger } from '@radix-ui/react-dialog';
+import { Button } from '../ui/button';
+import { CaretDownIcon } from '@radix-ui/react-icons';
+import Modal from "../Modal";
 
 type TokenSelectorProps = {
   tokenLogo: string;
@@ -8,32 +12,23 @@ type TokenSelectorProps = {
 };
 
 function TokenSelector(props: TokenSelectorProps) {
-  const [isOpen, toggle] = useState(false);
+  const [open, setOpen] = useState<boolean>(false);
   const [search, setSearch] = useState<string>('');
 
-  function close() {
-    document.body.style.overflow = 'auto';
-    toggle(false);
-  }
-
   return (
-    <>
-      {isOpen && (
-        <div className="transition top-0 left-0 absolute bg-dagger0 z-10 w-screen h-screen flex justify-center items-center">
-          <div className="flex flex-col gap-4 bg-dagger1 border-dagger2 border border-solid p-4 rounded-lg max-w-md">
-            <div className="flex flex-row justify-between items-center">
-              <p className="font-bold text-base">Select a token</p>
-              <svg
-                onClick={() => close()}
-                className="w-5 h-5 cursor-pointer"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M6 18 18 6m0 12L6 6" />
-              </svg>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="secondary">
+          <div className="flex flex-row gap-2 items-center">
+            <div className="flex flex-row items-center">
+              <img src={props.tokenLogo} alt={props.tokenSymbol} className="rounded-full h-4 w-4 mr-2" />
+              {props.tokenSymbol}
+              <CaretDownIcon className="h-4 w-4 ml-1" />
             </div>
+          </div>
+        </Button>
+      </DialogTrigger>
+        <Modal isOpen={open} title="Select a token" toggle={setOpen}>
             <div className="flex flex-row gap-2 bg-dagger1 border-dagger2 border border-solid p-2 rounded-lg items-center">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <path className="text-dagger3" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
@@ -79,31 +74,8 @@ function TokenSelector(props: TokenSelectorProps) {
                 </div>
               ))}
             </div>
-          </div>
-        </div>
-      )}
-      <button
-        onClick={() => {
-          if (isOpen) {
-            document.body.style.overflow = 'auto';
-          } else {
-            document.body.style.overflow = 'hidden';
-          }
-
-          toggle(!isOpen);
-        }}
-      >
-        <div className="flex flex-row gap-2 items-center">
-          <div className="flex flex-row gap-1 items-center">
-            <img src={props.tokenLogo} alt={props.tokenSymbol} className="rounded-full size-4" />
-            <span className="text-base">{props.tokenSymbol}</span>
-          </div>
-          <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m19 9-7 7-7-7" />
-          </svg>
-        </div>
-      </button>
-    </>
+        </Modal>
+    </Dialog>
   );
 }
 
