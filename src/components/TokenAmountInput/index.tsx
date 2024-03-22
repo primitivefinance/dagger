ffbdb1b1c2bd2c54f75ae1aeb0db363c4acefab8
Kinrezc <matt.czernik@gmail.com>
2024-03-22 16:28:23 -0400

    cleanup input areas

--- src/components/TokenAmountInput/index.tsx
+++ src/components/TokenAmountInput/index.tsx
@@ -1,3 +1,8 @@
+import { CaretDownIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "@radix-ui/react-label";
+
 type TokenAmountInputProps = {
   tokenAddress: `0x${string}`;
   tokenSymbol: string;
@@ -7,36 +12,60 @@ type TokenAmountInputProps = {
   amount: string;
   setAmount: (amount: string) => void;
 };
-
+Label;
 function TokenAmountInput(props: TokenAmountInputProps) {
   return (
     <div className="grid grid-cols-2 bg-dagger1 border border-solid border-dagger2 p-3 rounded-xl gap-2 w-full">
-      <input value={props.amount} onChange={(e) => props.setAmount(e.target.value)} className="text-lg" placeholder="0.0" />
+      <Input
+        value={props.amount}
+        onChange={(e) => props.setAmount(e.target.value)}
+        placeholder="0.0"
+      />
       <div className="flex flex-row items-center justify-end">
-        <button>
-          <div className="flex flex-row gap-2 items-center">
-            <div className="flex flex-row gap-1 items-center">
-              <img src={props.tokenLogo} alt={props.tokenSymbol} className="rounded-full size-4" />
-              <span className="text-sm font-bold">{props.tokenSymbol}</span>
-            </div>
-            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m19 9-7 7-7-7" />
-            </svg>
+        <Button variant="secondary">
+          <div className="flex flex-row items-center">
+            <img
+              src={props.tokenLogo}
+              alt={props.tokenSymbol}
+              className="rounded-full w-4 h-4 mr-2"
+            />
+            {props.tokenSymbol}
+            <CaretDownIcon className="w-4 h-4 ml-1" />
           </div>
-        </button>
+        </Button>
       </div>
-      <p className="text-sm text-dagger3">${isNaN(parseFloat(props.amount)) ? '0.0' : (props.tokenPrice * parseFloat(props.amount)).toLocaleString(undefined)}</p>
+      <Label className="text-sm font-semibold ml-1">
+        $
+        {isNaN(parseFloat(props.amount))
+          ? "0.0"
+          : (props.tokenPrice * parseFloat(props.amount)).toLocaleString(
+              undefined
+            )}
+      </Label>
       <button className="p-0 border-0 hover:opacity-100 bg-transparent">
         <div className="flex flex-row gap-1 justify-end items-center group">
-          <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <path className="text-dagger3 group-hover:text-dagger4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z" />
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              className="text-dagger3 group-hover:text-dagger4"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M17 8H5m12 0c.6 0 1 .4 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4c.6 0 1-.4 1-1v-2c0-.6-.4-1-1-1Z"
+            />
           </svg>
-          <p
+          <Label
             className="text-sm text-dagger3 group-hover:text-dagger4"
             onClick={() => props.setAmount(props.tokenBalance.toString())}
           >
             {props.tokenBalance.toLocaleString(undefined)} {props.tokenSymbol}
-          </p>
+          </Label>
         </div>
       </button>
     </div>
@@ -44,3 +73,4 @@ function TokenAmountInput(props: TokenAmountInputProps) {
 }
 
 export default TokenAmountInput;
+
--- src/pages/create-pool/index.tsx
+++ src/pages/create-pool/index.tsx
@@ -1,44 +1,56 @@
-import { useEffect, useState } from 'react';
-import { useAccount, useConnect } from 'wagmi';
-import { isAddress, parseEther, parseUnits } from 'viem';
+import { useEffect, useState } from "react";
+import { useAccount, useConnect } from "wagmi";
+import { isAddress, parseEther, parseUnits } from "viem";
 
-import { balanceOf, allowance, approve } from '@/lib/erc20';
-import TokenAmountInput from '@/components/TokenAmountInput';
-import PoolConfigCard from '@/components/Card';
-import { computeAndFormatPrice, computePrice } from '@/lib/g3m';
-import TokenSelector from '@/components/TokenSelector';
-import { LogNormal, init, G3M, DFMM } from '@/lib/dfmm';
-import { tokens } from '@/data/tokens';
-import { title, subtitle, tags, strats, feeLevels, weights } from '@/data/copy/create-pool'
-import { usePrices } from '@/store/PricesContext';
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { balanceOf, allowance, approve } from "@/lib/erc20";
+import TokenAmountInput from "@/components/TokenAmountInput";
+import PoolConfigCard from "@/components/Card";
+import { computeAndFormatPrice, computePrice } from "@/lib/g3m";
+import TokenSelector from "@/components/TokenSelector";
+import { LogNormal, init, G3M, DFMM } from "@/lib/dfmm";
+import { tokens } from "@/data/tokens";
+import {
+  title,
+  subtitle,
+  tags,
+  strats,
+  feeLevels,
+  weights,
+} from "@/data/copy/create-pool";
+import { usePrices } from "@/store/PricesContext";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 
 function CreatePool() {
   const { address } = useAccount();
   const { connectors, connect } = useConnect();
   const { prices } = usePrices().state;
-  const { checkPrices } = usePrices()
+  const { checkPrices } = usePrices();
 
-  const [strategy, setStrategy] = useState<'G3M' | 'LogNormal'>('G3M');
+  const [strategy, setStrategy] = useState<"G3M" | "LogNormal">("G3M");
   const [tokenX, setTokenX] = useState<`0x${string}`>(tokens[4].address);
   const [tokenY, setTokenY] = useState<`0x${string}`>(tokens[3].address);
   const [weight, setWeight] = useState<number>(50);
   const [feeRate, setFeeRate] = useState<number>(0.3);
-  const [controller, setController] = useState<string>('');
-  const [reserveX, setReserveX] = useState<string>('');
-  const [reserveY, setReserveY] = useState<string>('');
+  const [controller, setController] = useState<string>("");
+  const [reserveX, setReserveX] = useState<string>("");
+  const [reserveY, setReserveY] = useState<string>("");
   const [tokenXBalance, setTokenXBalance] = useState<number>(0);
   const [tokenYBalance, setTokenYBalance] = useState<number>(0);
-
-  const [tokenAOpen, setTokenAOpen] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
       if (address) {
         const balance = await balanceOf(tokenX, address);
         setTokenXBalance(balance);
-        checkPrices(_tokenX.symbol) // add whenever we NEED to check prices
+        checkPrices(_tokenX.symbol); // add whenever we NEED to check prices
       }
     })();
   }, [address, tokenX]);
@@ -48,7 +60,7 @@ function CreatePool() {
       if (address) {
         const balance = await balanceOf(tokenY, address);
         setTokenYBalance(balance);
-        checkPrices(_tokenY.symbol)
+        checkPrices(_tokenY.symbol);
       }
     })();
   }, [address, tokenY]);
@@ -57,52 +69,57 @@ function CreatePool() {
   const _tokenY = tokens.find((token) => token.address === tokenY)!;
 
   return (
-    <div className="py-16 container mx-auto max-w-4xl gap-14 flex flex-col">
-
+    <div className="py-16 container mx-auto max-w-6xl gap-14 flex flex-col">
       <div className="flex flex-col gap-2">
         <h2 className="text-3xl font-semibold">{title}</h2>
-        <h4 className="leading-tight text-xl">
-          {subtitle}
-        </h4>
+        <h4 className="leading-tight text-xl">{subtitle}</h4>
       </div>
       <div className="grid grid-cols-[1fr_2fr] gap-x-14 gap-y-8">
         <Card>
           <CardHeader>
-          <CardTitle>{tags[0].title}</CardTitle>
-          <CardDescription>{tags[0].sub}</CardDescription>
+            <CardTitle>{tags[0].title}</CardTitle>
+            <CardDescription>{tags[0].sub}</CardDescription>
           </CardHeader>
         </Card>
 
         <div className="flex flex-row gap-4 items-center">
           <TokenSelector
-            tokenLogo={tokens.find((token) => token.address === tokenX)?.logo || ''}
-            tokenSymbol={tokens.find((token) => token.address === tokenX)?.symbol || ''}
+            tokenLogo={
+              tokens.find((token) => token.address === tokenX)?.logo || ""
+            }
+            tokenSymbol={
+              tokens.find((token) => token.address === tokenX)?.symbol || ""
+            }
             setToken={setTokenX}
           />
           <TokenSelector
-            tokenLogo={tokens.find((token) => token.address === tokenY)?.logo || ''}
-            tokenSymbol={tokens.find((token) => token.address === tokenY)?.symbol || ''}
+            tokenLogo={
+              tokens.find((token) => token.address === tokenY)?.logo || ""
+            }
+            tokenSymbol={
+              tokens.find((token) => token.address === tokenY)?.symbol || ""
+            }
             setToken={setTokenY}
           />
         </div>
 
         <Card>
           <CardHeader>
-          <CardTitle>{tags[1].title}</CardTitle>
-          <CardDescription>{tags[1].sub}</CardDescription>
+            <CardTitle>{tags[1].title}</CardTitle>
+            <CardDescription>{tags[1].sub}</CardDescription>
           </CardHeader>
         </Card>
 
         <div className="flex flex-row gap-4 items-center">
           <PoolConfigCard
-            isSelected={strategy === 'G3M'}
-            onClick={() => setStrategy('G3M')}
+            isSelected={strategy === "G3M"}
+            onClick={() => setStrategy("G3M")}
             title={strats[0].title}
             description={strats[0].sub}
           />
           <PoolConfigCard
-            isSelected={strategy === 'LogNormal'}
-            onClick={() => setStrategy('LogNormal')}
+            isSelected={strategy === "LogNormal"}
+            onClick={() => setStrategy("LogNormal")}
             title={strats[1].title}
             description={strats[1].sub}
           />
@@ -110,8 +127,8 @@ function CreatePool() {
 
         <Card>
           <CardHeader>
-          <CardTitle>{tags[2].title}</CardTitle>
-          <CardDescription>{tags[2].sub}</CardDescription>
+            <CardTitle>{tags[2].title}</CardTitle>
+            <CardDescription>{tags[2].sub}</CardDescription>
           </CardHeader>
         </Card>
 
@@ -146,14 +163,12 @@ function CreatePool() {
           />
         </div>
 
-
         <Card>
           <CardHeader>
-          <CardTitle>{tags[3].title}</CardTitle>
-          <CardDescription>{tags[3].sub}</CardDescription>
+            <CardTitle>{tags[3].title}</CardTitle>
+            <CardDescription>{tags[3].sub}</CardDescription>
           </CardHeader>
         </Card>
-
 
         <div className="flex flex-row gap-4 items-center">
           <PoolConfigCard
@@ -181,62 +196,68 @@ function CreatePool() {
 
         <Card>
           <CardHeader>
-          <CardTitle>{tags[4].title}</CardTitle>
-          <CardDescription>{tags[4].sub}</CardDescription>
+            <CardTitle>{tags[4].title}</CardTitle>
+            <CardDescription>{tags[4].sub}</CardDescription>
           </CardHeader>
         </Card>
 
-
-        <div className="flex flex-col">
-          <div className="flex flex-row justify-between items-center bg-dagger1 border border-solid border-dagger2 text-sm p-3 w-full rounded-xl">
-            <input value={controller} onChange={(e) => setController(e.target.value)} className="flex-grow" placeholder="Use an Ethereum address or an ENS name..." />
-            {isAddress(controller) ? (
-              <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="green" viewBox="0 0 24 24">
-                <path fillRule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clipRule="evenodd" />
-              </svg>
-            ) : (
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <path stroke="red" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M6 18 18 6m0 12L6 6" />
-              </svg>
-            )}
+        <div className="grid w-full items-center gap-1">
+          <Label className="text-sm font-semibold">Controller address</Label>
+          <Input
+            value={controller}
+            onChange={(e) => setController(e.target.value)}
+            placeholder="Use an Ethereum address or an ENS name..."
+          />
+          <div className="w-full flex justify-end -my-2">
+            <Button
+              variant="link"
+              className="p-0"
+              onClick={() =>
+                setController(address !== undefined ? address : "")
+              }
+            >
+              Use my current wallet address
+            </Button>
           </div>
-
-          <button
-            className="flex justify-end bg-transparent border-0"
-            onClick={() => setController(address !== undefined ? address : '')}
-          >
-            Use my current wallet address
-          </button>
         </div>
-
 
         <Card>
           <CardHeader>
-          <CardTitle>{tags[5].title}</CardTitle>
-          <CardDescription>{tags[5].sub}</CardDescription>
+            <CardTitle>{tags[5].title}</CardTitle>
+            <CardDescription>{tags[5].sub}</CardDescription>
           </CardHeader>
         </Card>
 
-
         <div className="flex flex-col gap-4 items-start">
-
           <TokenAmountInput
-            tokenAddress={tokens.find((token) => token.address === tokenX)!.address}
-            tokenSymbol={tokens.find((token) => token.address === tokenX)!.symbol}
+            tokenAddress={
+              tokens.find((token) => token.address === tokenX)!.address
+            }
+            tokenSymbol={
+              tokens.find((token) => token.address === tokenX)!.symbol
+            }
             tokenBalance={tokenXBalance}
             amount={reserveX}
             setAmount={setReserveX}
-            tokenPrice={prices[tokens.find((token) => token.address === tokenX)!.symbol]}
+            tokenPrice={
+              prices[tokens.find((token) => token.address === tokenX)!.symbol]
+            }
             tokenLogo={tokens.find((token) => token.address === tokenX)!.logo}
           />
 
           <TokenAmountInput
-            tokenAddress={tokens.find((token) => token.address === tokenY)!.address}
-            tokenSymbol={tokens.find((token) => token.address === tokenY)!.symbol}
+            tokenAddress={
+              tokens.find((token) => token.address === tokenY)!.address
+            }
+            tokenSymbol={
+              tokens.find((token) => token.address === tokenY)!.symbol
+            }
             tokenBalance={tokenYBalance}
             amount={reserveY}
             setAmount={setReserveY}
-            tokenPrice={prices[tokens.find((token) => token.address === tokenX)!.symbol]}
+            tokenPrice={
+              prices[tokens.find((token) => token.address === tokenX)!.symbol]
+            }
             tokenLogo={tokens.find((token) => token.address === tokenY)!.logo}
           />
 
@@ -244,11 +265,27 @@ function CreatePool() {
             <p className="text-sm">Price based on the given pool parameters:</p>
 
             <p className="text-base font-bold">
-              {computeAndFormatPrice(parseFloat(reserveX), parseFloat(reserveY), weight / 100, (100 - weight) / 100)} {_tokenY.symbol} <span className="text-xs font-normal">per {_tokenX.symbol}, </span> {computeAndFormatPrice(parseFloat(reserveY), parseFloat(reserveX), (100 - weight) / 100, weight / 100)} {_tokenX.symbol} <span className="text-xs font-normal">per {_tokenY.symbol}.</span>
+              {computeAndFormatPrice(
+                parseFloat(reserveX),
+                parseFloat(reserveY),
+                weight / 100,
+                (100 - weight) / 100
+              )}{" "}
+              {_tokenY.symbol}{" "}
+              <span className="text-xs font-normal">
+                per {_tokenX.symbol},{" "}
+              </span>{" "}
+              {computeAndFormatPrice(
+                parseFloat(reserveY),
+                parseFloat(reserveX),
+                (100 - weight) / 100,
+                weight / 100
+              )}{" "}
+              {_tokenX.symbol}{" "}
+              <span className="text-xs font-normal">per {_tokenY.symbol}.</span>
             </p>
           </div>
         </div>
-
       </div>
 
       <Button
@@ -270,21 +307,28 @@ function CreatePool() {
             }
 
             const result = await init(
-              strategy === 'G3M' ? G3M : LogNormal,
+              strategy === "G3M" ? G3M : LogNormal,
               tokenX,
               tokenY,
               parseUnits(reserveX, _tokenX.decimals),
-              parseEther(computePrice(parseFloat(reserveX), parseFloat(reserveY), weight / 100, (100 - weight) / 100).toString()),
+              parseEther(
+                computePrice(
+                  parseFloat(reserveX),
+                  parseFloat(reserveY),
+                  weight / 100,
+                  (100 - weight) / 100
+                ).toString()
+              ),
               parseEther((weight / 100).toString()),
               parseEther((feeRate / 100).toString()),
-              controller as `0x${string}`,
+              controller as `0x${string}`
             );
 
             console.log(result);
           }
         }}
       >
-        {address === undefined ? 'Connect Wallet' : 'Create Pool'}
+        {address === undefined ? "Connect Wallet" : "Create Pool"}
       </Button>
     </div>
   );
