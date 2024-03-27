d9cb0d773f3d2abfb03e53eaf8a8fa12a50ba364
kinrezc <matt.czernik@gmail.com>
2024-03-27 15:24:14 -0400

    watch command for codegen

--- codegen.ts
+++ codegen.ts
@@ -4,6 +4,7 @@ const config: CodegenConfig = {
   overwrite: true,
   schema: "https://dfmm-indexer-production-9708.up.railway.app/",
   documents: ["src/**/*.ts", "src/**/*.tsx"],
+  ignoreNoDocuments: true,
   generates: {
     "src/gql/": {
       preset: "client",
