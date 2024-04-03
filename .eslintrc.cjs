830e77ba4b4d97dcd9fabb38a3bf693c2bcea012
Zachary Thielemann <zachdt@gmail.com>
2024-04-03 15:56:56 -0400

    fix(eslint): full eslint/prettier setup.  cant live without it

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,41 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+  "extends": [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  "overrides": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
   },
+  "plugins": [ "react", "@typescript-eslint", "react-hooks" ],
+  "rules": {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": [ "error" ],
+    "react/jsx-filename-extension": [ "warn", { "extensions": [ ".tsx" ] } ],
+    "import/extensions": [ "error", "ignorePackages", { "ts": "never", "tsx": "never" } ],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": [ "error" ],
+    "@typescript-eslint/explicit-function-return-type": [ "error", { "allowExpressions": true } ],
+    "max-len": [ "warn", { "code": 100, "ignoreComments": true, "ignoreUrls": true } ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/prefer-default-export": "off",
+    "react/prop-types": "off",
+    "prettier/prettier": [ "error", { "endOfLine": "auto" } ]
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": {}
+    }
+  }
 }
